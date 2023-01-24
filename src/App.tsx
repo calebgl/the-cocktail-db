809117a1af782ce42101b86dbbe069e76a0208f1
calebgl <caleb.csgl@gmail.com>
2023-01-24 16:05:35 -0600

    build: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from '@components/Home'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          onClick={() => {
-            setCount(count => count + 1)
-          }}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Router>
+      <nav></nav>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/about">
+          <Home />
+        </Route>
+        <Route path="/cocktail/:id">
+          <Home />
+        </Route>
+        <Route path="/*">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
-
-export default App
